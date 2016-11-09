6564dcbd2cece8d837c1bf5e0d671458a8f3a135
YoungChrisV <contact@chrisv.be>
2016-11-09 21:15:54 +0100

    Implementing the instance variables

---
+++ app/src/net/lifeinvader/app/model/Market.java
@@ -0,0 +1,22 @@
+package net.lifeinvader.app.model;
+
+/**
+ * create and Market object
+ * @author <a href="mailto:contact@chrisv.be">Christophe Van Waesberghe (contact@chrisv.be)</a>
+ * @author <a href="mailto:delfosse.da@gmail.com">Danielle Delfosse (delfosse.da@gmail.com)</a>
+ *
+ */
+public class Market {
+	
+	String name = "N/A";
+	String code = "N/A";
+	
+	int popNoInternet = 0;
+	int popInternet = 0;
+	
+	int nbInscrit = 0;
+	
+	
+			
+
+}
