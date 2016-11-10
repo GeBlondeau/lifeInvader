23c7942300e4ccb896e05dfc64a50cc3dd77193a
Christophe Van Waesberghe <contact@chrisv.be>
2016-11-10 12:21:53 +0100

    Updating Market class

--- app/src/net/lifeinvader/app/model/Market.java
+++ app/src/net/lifeinvader/app/model/Market.java
@@ -1,7 +1,7 @@
 package net.lifeinvader.app.model;
 
 /**
- * create and Market object
+ * create an Market object TODO: Change the class description, it has to tell what Market does. 
  * @author <a href="mailto:contact@chrisv.be">Christophe Van Waesberghe (contact@chrisv.be)</a>
  * @author <a href="mailto:delfosse.da@gmail.com">Danielle Delfosse (delfosse.da@gmail.com)</a>
  *
@@ -11,10 +11,18 @@ public class Market {
 	String name = "N/A";
 	String code = "N/A";
 	
-	int popNoInternet = 0;
-	int popInternet = 0;
+	int popNoWeb = 0;
+	int popWeb = 0;
 	
 	int nbInscrit = 0;
+	
+	public Market(String name, String code, int popNoWeb, int popWeb){
+		this.name=name;
+		this.code=code;
+		this.popNoWeb=popNoWeb;
+		this.popWeb=popWeb;
+		
+	}
 	
 	
 			
