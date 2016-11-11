8d3dbe1123780ea238a15f1c6a46098e5018a12d
Christophe Van Waesberghe <contact@chrisv.be>
2016-11-11 20:07:13 +0100

    Package Event created

---
+++ app/src/net/lifeinvader/app/model/event/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * lifeInvader event package
+ * @author <a href="mailto:contact@chrisv.be">Christophe Van Waesberghe (contact@chrisv.be)</a>
+ * @author <a href="mailto:delfosse.da@gmail.com">Danielle Delfosse (delfosse.da@gmail.com)</a>
+ *
+ */
+package net.lifeinvader.app.model.event;
