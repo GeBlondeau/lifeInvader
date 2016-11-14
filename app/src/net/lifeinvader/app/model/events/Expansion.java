3e7662c30d0658d58da5663c222bc9a0fe37d2da
YoungChrisV <contact@chrisv.be>
2016-11-14 16:39:41 +0100

    Adding a constructor into Expansion

--- app/src/net/lifeinvader/app/model/events/Expansion.java
+++ app/src/net/lifeinvader/app/model/events/Expansion.java
@@ -7,6 +7,15 @@ public class Expansion extends Event{
 	private int quantity;
 	private int level; 
 	
+	public Expansion(int reputationCoef, int privacyCoef, int viralityCoef, int reward, String name, String desc, int price, int quantity, int level) {
+		super(reputationCoef, privacyCoef, viralityCoef, reward);
+		this.name=name;
+		this.desc=desc;
+		this.price=price;
+		this.quantity=quantity;
+		this.level=level;
+	}
+	
 }
 
 
