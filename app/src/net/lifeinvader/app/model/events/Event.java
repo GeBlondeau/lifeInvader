4911d8510b315d18488f10aaed0c427d32bcc590
YoungChrisV <contact@chrisv.be>
2016-11-14 16:35:31 +0100

    Adding a full constructor and toString function

--- app/src/net/lifeinvader/app/model/events/Event.java
+++ app/src/net/lifeinvader/app/model/events/Event.java
@@ -1,8 +1,19 @@
 package net.lifeinvader.app.model.events;
 
 public abstract class Event {
-	private int reputationCoef;
-	private int privacyCoef;
-	private int viralityCoef;
-	private int reward;
+	private int reputationCoef=0;
+	private int privacyCoef=0;
+	private int viralityCoef=0;
+	private int reward=0;
+	
+	public Event(int reputationCoef, int privacyCoef, int viralityCoef, int reward){
+		this.reputationCoef=reputationCoef;
+		this.privacyCoef=privacyCoef;
+		this.viralityCoef=viralityCoef;
+		this.reward=reward;	
+	}
+	
+	public String toString(){
+		return "Event["+this.reputationCoef+", "+this.privacyCoef+" ,"+this.viralityCoef+","+this.reward+" ]";
+	}
 }
