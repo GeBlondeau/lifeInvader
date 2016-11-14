2f835947e1810606ece4cf5062eab925df3a301a
YoungChrisV <contact@chrisv.be>
2016-11-14 17:06:13 +0100

    Adding the full constructor

--- app/src/net/lifeinvader/app/model/events/PopupBonus.java
+++ app/src/net/lifeinvader/app/model/events/PopupBonus.java
@@ -1,5 +1,15 @@
 package net.lifeinvader.app.model.events;
+import src.net.lifeinvader.app.model.Market;
+
 
 public class PopupBonus extends Event{
-	
+
+	Market market;
+
+	public PopupBonus(Market market,int reputationCoef, int privacyCoef, int viralityCoef, int reward) {
+		super(reputationCoef, privacyCoef, viralityCoef, reward);
+		
+		this.market=market;
+	}
 }
+
