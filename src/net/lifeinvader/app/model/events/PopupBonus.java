bbca6e7ce34ee3091e32d563ceb4a4141ad08c47
Danielle <danielledelfosse95@gmail.com>
2016-11-17 16:08:08 +0100

    Adding Constructors, Getters & Setters & toString method

--- src/net/lifeinvader/app/model/events/PopupBonus.java
+++ src/net/lifeinvader/app/model/events/PopupBonus.java
@@ -6,10 +6,36 @@ public class PopupBonus extends Event{
 
 	private Market market;
 
-	public PopupBonus(Market market,int reputationCoef, int privacyCoef, int viralityCoef, int reward) {
+	//Constructors 
+	public PopupBonus(int reputationCoef, int privacyCoef, int viralityCoef, int reward, Market market) {
 		super(reputationCoef, privacyCoef, viralityCoef, reward);
-		
-		this.market=market;
+		this.market = market;
 	}
+	
+	@Override
+	public String toString() {
+		return "PopupBonus [market=" + market + "]";
+	}
+	
+	//Getter(s) and Setter(s)
+	/**
+	 * 
+	 * @return
+	 */
+	public Market getMarket() {
+		return market;
+	}
+	
+	/**
+	 * 
+	 * @param market
+	 */
+	public void setMarket(Market market) {
+		this.market = market;
+	}
+
+	
+	
+	
 }
 
